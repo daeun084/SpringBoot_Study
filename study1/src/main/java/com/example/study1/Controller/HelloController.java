94e3af8f1486afdf518718eb260ce242ae0e18b6
daeun084 <030804jk@naver.com>
2023-09-06 20:12:19 +0900

    section2

--- study1/src/main/java/com/example/study1/Controller/HelloController.java
+++ study1/src/main/java/com/example/study1/Controller/HelloController.java
@@ -4,9 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+
     @GetMapping("hello")
     public String hello(Model model){
         model.addAttribute("data", "hello~");
@@ -18,6 +20,35 @@ public class HelloController {
         model.addAttribute("name", name);
         //웹에서 "name"을 파라미터로 받아옴
         //"name" 파라미터를 name으로 변경
+        //Key: "name", Value: name
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+
+        //Hello라는 클래스를 만든 후
+        //web에서 name이라는 파라미터를 받아 hello객체의 name으로 설정
+        //hello 객체를 반환
+    }
+
+    static class Hello{
+        private String name;
+        public String getName(){
+            return name;
+        }
+        public void setName(String name){
+            this.name = name;
+        }
     }
 }
