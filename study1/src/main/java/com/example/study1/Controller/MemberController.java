d697b9ec7eaffc82eef27310409c2644d9ce8a98
daeun084 <030804jk@naver.com>
2023-09-07 17:31:09 +0900

    Section4

--- study1/src/main/java/com/example/study1/Controller/MemberController.java
+++ study1/src/main/java/com/example/study1/Controller/MemberController.java
@@ -8,15 +8,13 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class MemberController {
-    private final MemberService meberService;
+    private MemberService meberService;
 
     //생성자를 사용해 MemberService class를 container에 등록
-    //MemberService를 스프링빈에 등록하기 위해 @Service 사용
     @Autowired
     //container에 있는 memberService를 가져와 controller와 연결시켜줌 -> Dependency Injection
     public MemberController(MemberService meberService) {
         this.meberService = meberService;
     }
-
 
 }
