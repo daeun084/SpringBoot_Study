6a7f362f2b5858328664e68931559a2e90346bc0
daeun084 <030804jk@naver.com>
2023-09-29 17:22:56 +0900

    JPA기본_Section3

--- jpa_basic/ex1_hello_jpa/src/main/java/hellojpa/JpaMain.java
+++ jpa_basic/ex1_hello_jpa/src/main/java/hellojpa/JpaMain.java
@@ -18,11 +18,29 @@ public class JpaMain {
         try {
             List<Member> findMembers = em.createQuery("select m from Member", Member.class).getResultList();
 
+
+            //비영속
+            Member member = new Member();
+            member.setName("helloA");
+            member.setId(1L);
+
+            //영속
+            em.persist(member);
+                //member가 영속성 컨텍스트에 저장함
+
+            //준영속
+            em.detach(member); //영속 컨텍스트에서 member를 분리
+
+            //삭제
+            em.remove(member); //DB에서 객체를 지움
+
             transaction.commit();
             //모든 데이터 변경은 Transaction 내부에서 일어나야 함
             //Transaction commit 시점에서
             //데이터 변경 유무를 확인하고
             //데이터가 바뀐 부분이 있다면 update query를 날려 update
+
+
         }catch (Exception e){
             transaction.rollback();
         }finally {
--- jpa_basic/ex1_hello_jpa/src/main/java/hellojpa/Member.java
+++ jpa_basic/ex1_hello_jpa/src/main/java/hellojpa/Member.java
@@ -1,30 +1,49 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
 
 @Entity
 //JPA를 사용하는 객체임을 명시 -> JPA가 관리
 public class Member {
     @Id //PK
     private long id;
-    private String name;
 
-    public void setId(long id) {
-        this.id = id;
+    //@Column(name = "name", insertable = true, updatable = true)
+    @Column(name = "name", columnDefinition = "varchar(100) default 'EMPTY'")
+    private String username;
+    //객체 필드 이름은 username이나 DB에는 name으로 저장
+
+    //@Column()
+    //private BigDecimal age1;
+    //아주 큰 숫자나 소수점 쓸 때 사용
+
+    private Integer age;
+    //DB에서 Integer와 가장 유사한 타입으로 만들어짐
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+    //enum 맵핑 사용하는 annotation
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+    //날짜 타입 매핑
+    //Type 3가지 -> DATE, TIME, TIMESTAMP
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+    //DB에 큰  컨텐츠를 넣음을 명시
+
+    @Transient
+    private int tmp;
+    //DB와 맵핑하고 싶지 않을 때 사용 (메모리에서만 사용)
+
+
+    public Member() {
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
 
 }
